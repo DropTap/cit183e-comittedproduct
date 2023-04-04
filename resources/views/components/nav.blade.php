d9a6c13843b8b084cb883817a607800b54a78a60
DropTap <radiantmintgaming@gmail.com>
2023-04-04 09:33:02 +0800

    first commit

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary mb-5">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="nerds.png" alt="" width="120" height="120"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <x-items/>
+  </div>
+</nav>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+    <footer class="text-white bg-primary">
+    <div class=" text-center">
+  <div class="card-body text-white bg-primary mb-3">
+    <p> HOME</p>
+    <h5 class="card-title">BLUE EAGLE INNOVATIONS 2023</h5>
+    <p class="card-text">"BlueEagle Innovations develops cutting-edge solutions to help businesses stay ahead."</p>
+    <a href="#" class="btn btn-dark">Pilgrimage</a>
+  </div>
+</div>
+    </footer>
+</body>
+</html>
